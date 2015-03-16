6b8985af020f25b7dfbf3869d2b28e62a78ea5f7
xin <zhouxinasl@gmail.com>
2015-03-16 18:25:03 +0800

    fix check hour act start time and end time

--- jhs_crawler/JHSBrandCheck.py
+++ jhs_crawler/JHSBrandCheck.py
@@ -58,7 +58,7 @@ class JHSBrandCheck():
             for act_r in act_results:
                 # 只抓时尚女士,精品男士
                 if int(act_r[2]) == 261000 or int(act_r[2]) == 262000:
-                    act_valList.append((str(act_r[1]),act_r[7],act_r[8],self.begin_time))
+                    act_valList.append((str(act_r[1]),act_r[7],act_r[8],self.begin_time,str(act_r[28]),str(act_r[29])))
                     if not self.act_dict.has_key(str(act_r[1])):
                         self.act_dict[str(act_r[1])] = []
                     # 按照活动Id找出商品
