663ae149781ea68292ba98f51d0705d9e0622726
xin <zhouxinasl@gmail.com>
2015-03-23 14:14:14 +0800

    fix history is not string

--- jhs_crawler/JHSItem.py
+++ jhs_crawler/JHSItem.py
@@ -208,7 +208,7 @@ class JHSItem():
 
             if re.search(r'<title>【聚划算】无所不能聚</title>', page, flags=re.S):
                 raise Common.NoPageException("# itemConfig: not find ju item page, redirecting to juhuasuan home,juid:%s,item_ju_url:%s"%(str(self.item_juId), self.item_ju_url))
-            elif type(self.crawler.history) is list and len(self.crawler.history) != 0 and re.search(r'302',self.crawler.history[0]):
+            elif type(self.crawler.history) is list and len(self.crawler.history) != 0 and re.search(r'302',str(self.crawler.history[0])):
                 raise Common.NoPageException("# itemConfig: not find ju item page, redirecting to other page,juid:%s,item_ju_url:%s"%(str(self.item_juId), self.item_ju_url))
             self.item_juPage = page
         else:
