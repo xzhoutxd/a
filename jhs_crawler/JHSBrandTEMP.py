f4df8b33d18c290ca1190dd49aca98850679aba8
xin <zhouxinasl@gmail.com>
2015-03-24 20:16:39 +0800

    fix TEMP

--- jhs_crawler/JHSBrandTEMP.py
+++ jhs_crawler/JHSBrandTEMP.py
@@ -43,7 +43,7 @@ class JHSBrandTEMP():
                     b_url_valList = self.activityListTemp3(page)
                 else:
                     #m = re.search(r'"firstFloorData":{"brandList":\[{.+?}}\],.+?},"floorList"', page, flags=re.S)
-                    m = re.search(r'=(\[{.+?"firstFloorData":{"brandList".+?},"floorList".+?}\]);', page, flags=re.S)
+                    m = re.search(r'=(\[{.+?"firstFloorData".+?,"floorList".+?}\]);', page, flags=re.S)
                     if m:
                         b_url_valList = self.activityListTemp4(m.group(1))
                     else:
