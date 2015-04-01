460b27c5f0935f54f872d65c89cf19968c1eab08
xin <zhouxinasl@gmail.com>
2015-04-01 18:19:45 +0800

    add run data

---
+++ jhs_crawler/rync/run_jhs_day.sh
@@ -0,0 +1,13 @@
+today=`date +"%Y-%m-%d"`
+
+if [ $# = 0 ]; then 
+	p_crawl_date=$today
+else
+	p_crawl_date=$1
+fi
+
+# day
+mysql -h 192.168.1.113 -u jhs -p123456 jhs <<EOF
+	call sp_jhs_stat_day(p_crawl_date);
+EOF
+exit;
---
+++ jhs_crawler/rync/run_jhs_hour.sh
@@ -0,0 +1,20 @@
+today=`date +"%Y-%m-%d"`
+hour=`date +"%H"`
+
+if [ $# = 0 ]; then 
+	p_crawl_date=$today
+	str_hour=$hour
+else
+	p_crawl_date=$1
+	str_hour=$2
+fi
+
+# dim
+# hour
+mysql -h 192.168.1.113 -u jhs -p123456 jhs <<EOF
+	call sp_jhs_stat_dim(p_crawl_date,str_hour);
+	call sp_jhs_stat_hour(p_crawl_date,str_hour);
+EOF
+exit;
+
+
