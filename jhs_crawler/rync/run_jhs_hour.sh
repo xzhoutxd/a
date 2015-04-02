1fb476a3581bb6d0559d677a39f9e6c5e3fab6f7
xin <zhouxinasl@gmail.com>
2015-04-02 16:09:23 +0800

    fix run day

--- jhs_crawler/rync/run_jhs_hour.sh
+++ jhs_crawler/rync/run_jhs_hour.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 today=`date +"%Y-%m-%d"`
 hour=`date +"%H"`
+#运行统计脚本
+now_time=$(date +%Y-%m-%d-%H:%M:%S)
+echo "-- run start...time:$now_time"
 if [ $# = 0 ]; then
         p_crawl_date=$today
         str_hour=$hour
@@ -9,8 +12,13 @@ else
         str_hour=$2
 fi
 
-mysql -h 192.168.1.113 -u jhs -p123456 jhs <<EOF
-       call sp_jhs_stat_dim($p_crawl_date,$str_hour);
-       call sp_jhs_stat_hour($p_crawl_date,$str_hour);
+echo "date:$p_crawl_date"
+echo "hour:$str_hour"
+
+mysql -h 192.168.1.113 -u jhs -p123456 jhs<<EOF
+       call sp_jhs_stat_dim('$p_crawl_date',$str_hour);
+       call sp_jhs_stat_hour('$p_crawl_date',$str_hour);
 EOF
+now_time=$(date +%Y-%m-%d-%H:%M:%S)
+echo "-- run end...time:$now_time"
 exit;
